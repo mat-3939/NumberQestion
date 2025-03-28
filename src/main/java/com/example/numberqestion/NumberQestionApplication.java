d05a731e0a6dc2bbc21ea55f346f57c6f8967087
mat-3939 <znuakierkto@ymail.ne.jp>
2025-03-28 13:39:11 +0900

    test2

--- src/main/java/com/example/numberqestion/NumberQestionApplication.java
+++ src/main/java/com/example/numberqestion/NumberQestionApplication.java
@@ -1,16 +1,26 @@
 package com.example.numberqestion;
 
+// 必要なSpring Bootのインポート
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// Lombokのアノテーション用のインポート
 import lombok.RequiredArgsConstructor;
 
-@SpringBootApplication
-@RequiredArgsConstructor
+/**
+ * Spring Bootアプリケーションのメインクラス
+ * このクラスがアプリケーションのエントリーポイントとなります
+ */
+@SpringBootApplication  // Spring Bootアプリケーションとして認識させるアノテーション
+@RequiredArgsConstructor  // Lombokによる初期化用コンストラクタを自動生成
 public class NumberQestionApplication 
 {
-
+	/**
+	 * アプリケーションのメインメソッド
+	 * Spring Bootアプリケーションを起動します
+	 * 
+	 * @param args コマンドライン引数
+	 */
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(NumberQestionApplication.class, args);
